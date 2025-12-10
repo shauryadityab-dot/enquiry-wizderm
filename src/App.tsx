6398f4bde37bc9d0777d4a21c5d21b63d36588bb
Shauryaditya <shauryadityab@gmail.com>
2025-12-10 13:36:19 +0530

    whs add

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
-import Whs from "./pages/WhsPage";
+import Whs from "./pages/Whs";
 
 const queryClient = new QueryClient();
 
